f0521d2d1f0e42b83fa893594a308ba6fedffd9e
evg-zlg <evg.zalogin@gmail.com>
2022-09-02 16:38:06 +0500

    feat: added header, main-section, footer

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+
+
 export default function MainPage() {
   return (
     <>  
-      <h1>ecodecor</h1>
+      <main className="wrapper">
+        <header className="header">
+          <figure className="header__logo logo">
+
+          </figure>
+        </header>
+        <h1 className="title">
+          Плетеные корзины · Декор · Фотореквизит
+        </h1>
+        <section className="section-one">
+
+        </section>
+        <footer className="footer">
+          
+        </footer>
+      </main>
     </>
   )
 }
